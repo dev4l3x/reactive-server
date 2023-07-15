f4b7a8ebad6cc5c64c7ec8ce7a3ab73aa0497495
Alex <asiglesias478@gmail.com>
2023-07-15 19:47:41 +0200

    Accept two concurrent request

--- src/main/java/com/dev4l3x/App.java
+++ src/main/java/com/dev4l3x/App.java
@@ -1,37 +1,64 @@
 package com.dev4l3x;
 
+import java.util.*;
 import java.io.IOError;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 
-/**
- * Hello world!
- *
- */
 public class App {
     public static void main(String[] args) {
-        try (ServerSocketChannel socketChannel = ServerSocketChannel.open()) {
+        try (ServerSocketChannel serverSocketChannel = ServerSocketChannel.open()) {
+
+            Selector selector = Selector.open();
 
             System.out.println("⏳ Litening for requests...");
-            socketChannel.bind(new InetSocketAddress(8080));
+            serverSocketChannel.bind(new InetSocketAddress(8080));
+            serverSocketChannel.configureBlocking(false);
+
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
             while (true) {
-                SocketChannel client = socketChannel.accept();
+                selector.select();
+                Set<SelectionKey> selectedKeys = selector.selectedKeys();
 
-                System.out.println("Incoming request!");
+                for (SelectionKey key : selectedKeys) {
 
-                String message = "Hello World!";
-                String response = String.format("HTTP/1.1 200 OK\nContent-Type: text/plain;\nContent-Length: %s\n\n%s",
-                        message.getBytes().length, message);
+                    if (key.isAcceptable()) {
+                        SocketChannel socketChannel = serverSocketChannel.accept();
+                        if (socketChannel == null) {
+                            continue;
+                        }
+                        socketChannel.configureBlocking(false);
+                        socketChannel.register(selector, SelectionKey.OP_READ);
+                        continue;
+                    }
 
-                System.out.println("⏳ Sending response to client...");
-                client.write(ByteBuffer.wrap(response.getBytes()));
-                client.close();
+                    if (!key.isReadable()) {
+                        continue;
+                    }
+
+                    SocketChannel client = (SocketChannel) key.channel();
+
+                    System.out.println("Incoming request!");
+
+                    String message = "Hello World!";
+                    String response = String.format(
+                            "HTTP/1.1 200 OK\nContent-Type: text/plain;\nContent-Length: %s\n\n%s",
+                            message.getBytes().length, message);
+
+                    System.out.println("⏳ Sending response to client...");
+                    client.write(ByteBuffer.wrap(response.getBytes()));
+                    client.close();
+                }
+
             }
-
         } catch (IOException exception) {
             System.err.println("An error has ocurred while creating socket");
         }
